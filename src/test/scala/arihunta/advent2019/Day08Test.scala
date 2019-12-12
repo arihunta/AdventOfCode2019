b9eda41dac371f59b8589d3e8e85c85062aaff5c
Shitesh Arihunta <arihunta@gmail.com>
2019-12-12 23:49:02 +0200

    Fix test name

--- src/test/scala/arihunta/advent2019/Day08Test.scala
+++ src/test/scala/arihunta/advent2019/Day08Test.scala
@@ -4,11 +4,11 @@ import org.scalatest.FunSuite
 
 class Day08Test extends FunSuite {
 
-    test("Day08-01") {
+    test("Day 08-01") {
         assert(Day08._01() == 2440)
     }
 
-    test("Day08-02") {
+    test("Day 08-02") {
         assert(Day08._02() ==
             " **  ****  **    **  **  " + "\n" +
             "*  *    * *  *    * *  * " + "\n" +
